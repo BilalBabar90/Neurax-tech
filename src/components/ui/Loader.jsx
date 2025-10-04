d10e361e9e96b112b792c486d6d2ca446e9adf11
arhamansari11 <arhamansari454647@gmail.com>
2025-10-04 22:14:09 +0500

    Added

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -28,19 +28,22 @@ const Loader = () => {
   // }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen " style={{marginTop : "190px"}}>
-      <AnimatePresence mode="wait">
-        <motion.h1
-          key={greetings[index]}
-          className="text-5xl md:text-7xl font-bold leading-tight text-center text-black"
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.8 }}
-          transition={{ duration: 0.4, ease: "easeInOut" }}
-        >
-          Hello
-        </motion.h1>
-      </AnimatePresence>
+    <div style={{backgroundColor : "black" , height:"100vh"}}>
+      <div className="flex items-center justify-center h-screen w-screen " style={{ paddingTop: "190px", backgroundColor: "black" }}>
+        <AnimatePresence mode="wait">
+          <motion.h1
+            key={greetings[index]}
+            className="text-5xl md:text-7xl font-bold leading-tight text-center text-black"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+            style={{ color: 'white' }}
+          >
+            Hello
+          </motion.h1>
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
