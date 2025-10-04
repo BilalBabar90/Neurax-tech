6a2e543282505b8cec4586c0330f01dfc4aef038
arhamansari11 <arhamansari454647@gmail.com>
2025-10-04 11:45:07 +0500

    Addde

---
+++ src/components/Image.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image1 from "../images/1.jpg"
+import Image2 from "../images/2.jpg"
+import Image3 from "../images/3.jpg"
+import Image4 from "../images/4.jpg"
+import Image5 from "../images/5.jpg"
+import Image6 from "../images/6.jpg"
+import Image7 from "../images/7.jpg"
+import Image8 from "../images/8.jpg"
+
+
+const Image = () => {
+    return (
+        <>
+
+        <img src={Image1}/>
+        </>
+    )
+}
+
+export default Image
